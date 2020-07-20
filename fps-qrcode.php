e77cfcdf615b3ea3d8c46bf9e8ad99e7ddc5e41a
Frey Mansikkaniemi <me@frey.hk>
2020-07-20 16:17:51 +0800

    Initial implementation

---
+++ fps-qrcode.php
@@ -0,0 +1,169 @@
+<?php
+require_once 'phpqrcode.php';
+
+header('Content-Type: image/png');
+
+if( $_GET["account"] && $_GET["bank_code"] && $_GET["mcc"] && $_GET["curr"] && $_GET["amount"] ){
+    
+
+    $data = array(
+        "account" => $_GET["account"],
+        "fps_id" => $_GET["fps_id"],
+        "bank_code" => $_GET["bank_code"],
+        "mobile" => $_GET["mobile"] ?? "",
+        "email" => $_GET["email"] ?? "",
+        "mcc" => $_GET["mcc"],
+        "currency" => $_GET["curr"],
+        "amount" =>  $_GET["amount"],
+        "reference" => $_GET["reference"] ?? ""
+    );
+
+
+    $qr_data = new ITS_FPS_QRCodeData($data);
+    
+    QRcode::png($qr_data->getDataString());
+}else{
+    QRcode::png('');
+}
+
+class ITS_FPS_QRCodeData {
+
+    public function __construct($data){
+        $this->data = $data;
+    }
+
+    public function getDataString(){
+        $msg = $this->emvString($this->data);
+        $crc = $this->pad(dechex($this->crc16ccitt($msg)),4);
+        return $msg . $crc;
+
+    }
+
+    private function pad($s, $size = 2) {
+        while (strlen($s) < $size) {
+            $s = "0" . $s;
+        }
+        return $s;
+    }
+
+    private function dataObject($id, $value){
+        $paddedLength = $this->pad('' . strlen($value), 2);
+        return $id . $paddedLength . $value;
+    }
+
+    private function emvString($data){
+        $payloadFormatIndicator = $this->dataObject("00","01");
+        $pointOfInitiationMethod = $this->dataObject("01", $data['amount'] === "" ? "11" : "12");
+        $guid = $this->dataObject("00","hk.com.hkicl");
+        $merchantAccountInformationTemplate = "";
+
+        switch($data['account']){
+            case "02":
+                $merchantAccountInformationTemplate = $this->dataObject("02", $data['fps_id']);
+                break;
+            case "03":
+                $merchantAccountInformationTemplate = $this->dataObject("01", $data['bank_code']) . $this->dataObject("03", $data['mobile']);
+                break;
+            case "04":
+                $merchantAccountInformationTemplate = $this->dataObject("01", $data['bank_code']) . $this->dataObject("04", strtoupper($data['email']));
+                break;
+            default:
+                return null;
+        }
+
+        $merchantAccountInformation = $this->dataObject("26", $guid . $merchantAccountInformationTemplate);
+        $merchantCategoryCode = $this->dataObject("52", $data['mcc']);
+        $transactionCurrency = $this->dataObject("53", $data['currency']);
+        $countryCode = $this->dataObject("58", "HK");
+        $merchantName = $this->dataObject("59","NA");
+        $merchantCity = $this->dataObject("60","HK");
+        $transactionAmount = $data['amount'] === '' ? "" : $this->dataObject("54",$data['amount']);
+        $reference = $data['reference'] === '' ? "" : $this->dataObject("05",$data['reference']);
+        $additionalDataTemplate = $reference === "" ? "" : $this->dataObject("62", $reference);
+
+        $msg = "";
+
+        $msg .= $payloadFormatIndicator;
+        $msg .= $pointOfInitiationMethod;
+        $msg .= $merchantAccountInformation;
+        $msg .= $merchantCategoryCode;
+        $msg .= $transactionCurrency;
+        $msg .= $countryCode;
+        $msg .= $merchantName;
+        $msg .= $merchantCity;
+        $msg .= $transactionAmount;
+        $msg .= $additionalDataTemplate;
+        $msg .= "6304";
+
+        return $msg;
+
+
+    }
+
+    private function crc16ccitt($s){
+        $crcTable = [
+            0x0000, 0x1021, 0x2042, 0x3063, 0x4084, 0x50a5,
+            0x60c6, 0x70e7, 0x8108, 0x9129, 0xa14a, 0xb16b,
+            0xc18c, 0xd1ad, 0xe1ce, 0xf1ef, 0x1231, 0x0210,
+            0x3273, 0x2252, 0x52b5, 0x4294, 0x72f7, 0x62d6,
+            0x9339, 0x8318, 0xb37b, 0xa35a, 0xd3bd, 0xc39c,
+            0xf3ff, 0xe3de, 0x2462, 0x3443, 0x0420, 0x1401,
+            0x64e6, 0x74c7, 0x44a4, 0x5485, 0xa56a, 0xb54b,
+            0x8528, 0x9509, 0xe5ee, 0xf5cf, 0xc5ac, 0xd58d,
+            0x3653, 0x2672, 0x1611, 0x0630, 0x76d7, 0x66f6,
+            0x5695, 0x46b4, 0xb75b, 0xa77a, 0x9719, 0x8738,
+            0xf7df, 0xe7fe, 0xd79d, 0xc7bc, 0x48c4, 0x58e5,
+            0x6886, 0x78a7, 0x0840, 0x1861, 0x2802, 0x3823,
+            0xc9cc, 0xd9ed, 0xe98e, 0xf9af, 0x8948, 0x9969,
+            0xa90a, 0xb92b, 0x5af5, 0x4ad4, 0x7ab7, 0x6a96,
+            0x1a71, 0x0a50, 0x3a33, 0x2a12, 0xdbfd, 0xcbdc,
+            0xfbbf, 0xeb9e, 0x9b79, 0x8b58, 0xbb3b, 0xab1a,
+            0x6ca6, 0x7c87, 0x4ce4, 0x5cc5, 0x2c22, 0x3c03,
+            0x0c60, 0x1c41, 0xedae, 0xfd8f, 0xcdec, 0xddcd,
+            0xad2a, 0xbd0b, 0x8d68, 0x9d49, 0x7e97, 0x6eb6,
+            0x5ed5, 0x4ef4, 0x3e13, 0x2e32, 0x1e51, 0x0e70,
+            0xff9f, 0xefbe, 0xdfdd, 0xcffc, 0xbf1b, 0xaf3a,
+            0x9f59, 0x8f78, 0x9188, 0x81a9, 0xb1ca, 0xa1eb,
+            0xd10c, 0xc12d, 0xf14e, 0xe16f, 0x1080, 0x00a1,
+            0x30c2, 0x20e3, 0x5004, 0x4025, 0x7046, 0x6067,
+            0x83b9, 0x9398, 0xa3fb, 0xb3da, 0xc33d, 0xd31c,
+            0xe37f, 0xf35e, 0x02b1, 0x1290, 0x22f3, 0x32d2,
+            0x4235, 0x5214, 0x6277, 0x7256, 0xb5ea, 0xa5cb,
+            0x95a8, 0x8589, 0xf56e, 0xe54f, 0xd52c, 0xc50d,
+            0x34e2, 0x24c3, 0x14a0, 0x0481, 0x7466, 0x6447,
+            0x5424, 0x4405, 0xa7db, 0xb7fa, 0x8799, 0x97b8,
+            0xe75f, 0xf77e, 0xc71d, 0xd73c, 0x26d3, 0x36f2,
+            0x0691, 0x16b0, 0x6657, 0x7676, 0x4615, 0x5634,
+            0xd94c, 0xc96d, 0xf90e, 0xe92f, 0x99c8, 0x89e9,
+            0xb98a, 0xa9ab, 0x5844, 0x4865, 0x7806, 0x6827,
+            0x18c0, 0x08e1, 0x3882, 0x28a3, 0xcb7d, 0xdb5c,
+            0xeb3f, 0xfb1e, 0x8bf9, 0x9bd8, 0xabbb, 0xbb9a,
+            0x4a75, 0x5a54, 0x6a37, 0x7a16, 0x0af1, 0x1ad0,
+            0x2ab3, 0x3a92, 0xfd2e, 0xed0f, 0xdd6c, 0xcd4d,
+            0xbdaa, 0xad8b, 0x9de8, 0x8dc9, 0x7c26, 0x6c07,
+            0x5c64, 0x4c45, 0x3ca2, 0x2c83, 0x1ce0, 0x0cc1,
+            0xef1f, 0xff3e, 0xcf5d, 0xdf7c, 0xaf9b, 0xbfba,
+            0x8fd9, 0x9ff8, 0x6e17, 0x7e36, 0x4e55, 0x5e74,
+            0x2e93, 0x3eb2, 0x0ed1, 0x1ef0
+        ];
+
+        $crc = 0xFFFF;
+        $j = null;
+
+        $slen = strlen($s);
+        for ($i=0; $i < $slen; $i++) { 
+            $c = ord(substr($s, $i));
+            if( $c > 255 ){
+                echo "Range error";
+                exit;
+            }
+            $j = ($c ^ ($crc >> 8)) & 0xFF;
+            $crc = $crcTable[$j] ^ ($crc << 8);
+        }
+
+        return (($crc ^ 0) & 0xFFFF);
+
+    }
+}
+
+?>
---
+++ is-woo-payment-fps.php
@@ -0,0 +1,311 @@
+<?php 
+/**
+ * @package Woo_Payment_FPS
+ * @version 1.0
+ */
+/*
+Plugin Name: Woo Payment FPS
+Plugin URI: http://invite.hk/
+Description: Woocommerce payment method enabling Hong Kong FPS payments. Displays QR code and FPS payent if to user. Requires manual confirmation.
+Author: Frey Mansikkaniemi, invITe Services
+Version: 1.0
+Author URI: http://frey.hk/
+License: MIT
+*/
+
+if (!defined('ABSPATH')) {
+    return;
+}
+
+/**
+ * Check if WooCommerce is active
+ **/
+if (
+    !in_array(
+        'woocommerce/woocommerce.php',
+        apply_filters('active_plugins', get_option('active_plugins'))
+    )
+) {
+    add_action('admin_notices', function () {
+        ?>
+            <div class="error notice">
+                <strong><?php __('Woo Payment FPS requires WooCommerce to be installed & activated.',ITS_WPF_PLUGIN_ID)?></strong>
+            </div>
+        <?php
+    });
+
+    return;
+}
+
+
+define('ITS_WPF_PLUGIN_ID','its_wpf_payment_gateway');
+
+/**
+ * Register payment gateway class
+ */
+
+add_filter( 'woocommerce_payment_gateways', 'its_wpf_add_class' );
+
+function its_wpf_add_class( $methods ){
+    $methods[] = 'WC_Gateway_Invite_FPS_Payment_Gateway';
+    return $methods;
+}
+
+/**
+ * Change text on the Pay order button.
+ */
+
+add_filter('woocommerce_available_payment_gateways', 'its_wpf_pay_order_label');
+function its_wpf_pay_order_label($gateways) {
+    if($gateways[ITS_WPF_PLUGIN_ID]) {
+        $gateways[ITS_WPF_PLUGIN_ID]->order_button_text = __('Confirm FPS Payment Completed',ITS_WPF_PLUGIN_ID);
+    }
+    return $gateways;
+}
+
+/**
+ * Change text on the Thank You page.
+ */
+
+add_filter('woocommerce_thankyou_' . ITS_WPF_PLUGIN_ID,  'its_wpf_thankyou', 10, 1);
+function its_wpf_thankyou($order_id){
+    global $woocommerce;
+    $order = wc_get_order( $order_id );
+
+    echo '<p>';
+    echo __('Your payment will be confirmed manually.',ITS_WPF_PLUGIN_ID);
+    echo '</p><p>';
+    echo __('FPS Transaction Reference: ',ITS_WPF_PLUGIN_ID) . $order->get_transaction_id();
+}
+
+/**
+ * Load payment gateway class
+ */
+
+add_action( 'plugins_loaded', 'its_wpf_init_gateway' );
+
+function its_wpf_init_gateway(){
+
+    class WC_Gateway_Invite_FPS_Payment_Gateway extends WC_Payment_Gateway {
+        public function __construct() {
+            $this->id = ITS_WPF_PLUGIN_ID;
+            $this->icon = plugins_url('assets/fps.png', __FILE__); 
+            $this->has_fields = true;
+            $this->method_title = __('Hong Kong Faster Payment System (FPS)',ITS_WPF_PLUGIN_ID);
+            $this->method_description = __("Hong Kong interbank real time transfer using account holder ids and QR codes.",ITS_WPF_PLUGIN_ID);
+            $this->supports = array(
+                'products'
+            );
+            $this->init_form_fields();
+            $this->init_settings();
+            $this->title = $this->get_option( 'title' );
+            $this->description = $this->get_option( 'description' );
+            $this->enabled = $this->get_option( 'enabled' );
+            $this->account_id_type = $this->get_option( 'account_id_type' );
+            $this->account_fps_id = $this->get_option( 'account_fps_id' );
+            $this->account_bank_code = $this->get_option( 'account_bank_code' );
+            $this->ask_to_pay = $this->get_option( 'ask_to_pay' );
+            $this->fps_payment_reference_guide = $this->get_option( 'fps_payment_reference_guide' );
+
+            add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+        }
+
+        public function init_form_fields() {
+            $this->form_fields = array(
+                'enabled' => array(
+                    'title'       => __('Enable/Disable',ITS_WPF_PLUGIN_ID),
+                    'label'       => __('Enable Hong Kong Faster Payment System',ITS_WPF_PLUGIN_ID),
+                    'type'        => 'checkbox',
+                    'description' => '',
+                    'default'     => 'no'
+                ),
+                'title' => array(
+                    'title'       => __('Title',ITS_WPF_PLUGIN_ID),
+                    'type'        => 'text',
+                    'description' => __('This controls the title which the user sees during checkout.',ITS_WPF_PLUGIN_ID),
+                    'default'     => __('Hong Kong FPS',ITS_WPF_PLUGIN_ID),
+                    'desc_tip'    => true
+                ),
+                'description' => array(
+                    'title'       => __('Description',ITS_WPF_PLUGIN_ID),
+                    'type'        => 'textarea',
+                    'description' => __('This controls the description which the user sees during checkout.',ITS_WPF_PLUGIN_ID),
+                    'default'     => __('Pay with HK Faster Payment System (FPS). Scan the presented QR code with your bank\'s app or enter the payee FPS id.',ITS_WPF_PLUGIN_ID),
+                ),
+                'account_id_type' => array(
+                    'title'       => __('Account Id Type',ITS_WPF_PLUGIN_ID),
+                    'type'        => 'select',
+                    'options'     => array(
+                        '02'    => __('FPS ID',ITS_WPF_PLUGIN_ID),
+                        '03'    => __('Mobile Phone Number',ITS_WPF_PLUGIN_ID),
+                        '04'     => __('Email Address',ITS_WPF_PLUGIN_ID)
+                    ),
+                    'default'    => '02'
+                ),
+                'account_fps_id' => array(
+                    'title'       => __('Account Id',ITS_WPF_PLUGIN_ID),
+                    'type'        => 'text',
+                    'description' => __('E-mail address, phone number or specific FPS id',ITS_WPF_PLUGIN_ID),
+                ),
+                'account_bank_code' => array(
+                    'title'       => __('Bank Code',ITS_WPF_PLUGIN_ID),
+                    'type'        => 'text',
+                    'description' => __('Three number Hong Kong bank code.',ITS_WPF_PLUGIN_ID)
+                ),
+                'fps_payment_reference_guide' => array(
+                    'title'       => __('Payment Reference Guide',ITS_WPF_PLUGIN_ID),
+                    'type'        => 'textarea',
+                    'default'     => __('Please input the payment reference number below after payment has been completed.',ITS_WPF_PLUGIN_ID),
+                    'description' => __('Instructions visible to the customer for providing payment reference number after payment. This is not visble of Ask to Pay is active.',ITS_WPF_PLUGIN_ID)
+                ),
+                'ask_to_pay' => array(
+                    'title'       => __('Ask to Pay Enabled',ITS_WPF_PLUGIN_ID),
+                    'label'       => __('Ask to Pay Enabled',ITS_WPF_PLUGIN_ID),
+                    'type'        => 'checkbox',
+                    'description' => __('The ask to pay function must be enabled by your bank in order to use payment reference numbers.',ITS_WPF_PLUGIN_ID),
+                    'default'     => 'no'
+                ),
+            );
+        }
+
+        private function fps_data($reference){
+
+            global $woocommerce;
+            
+            $currency_code = get_woocommerce_currency();
+
+            //echo $currency_code . '<br>';
+
+            $fps_currencies = array(
+                "HKD" => "344",
+                "CNY" => "156"
+            );
+
+            $fps_currency = $fps_currencies[$currency_code] ?? null;
+
+            if( !$fps_currency )
+                return null;
+
+            $data = array(
+                "account"   => $this->account_id_type,
+                "bank_code" => $this->account_bank_code,
+                "fps_id"    => $this->account_id_type === "02" ? $this->account_fps_id : "",
+                "mobile"    => $this->account_id_type === "03" ? $this->account_fps_id : "",
+                "email"     => $this->account_id_type === "04" ? $this->account_fps_id : "",
+                "mcc"       => "0000",
+                "curr"      => $fps_currency,
+                "amount"    => '' . $this->get_order_total(),
+                "reference" => $this->ask_to_pay === 'yes' ? $reference : ""
+
+            );
+
+            return $data;
+        }
+
+        private function urlencode_array($array){
+            $url = "";
+            $delimiter = "";
+
+            foreach ($array as $key => $value) {
+                if($value !== ""){
+                    $url .= $delimiter . $key . '=' .  urlencode($value);
+                    $delimiter = "&";
+                }
+            }
+
+            return $url;
+        }
+
+        public function payment_fields() {
+
+            global $wp;
+
+            $fps_ref_string = $wp->query_vars['order-pay'] ?? $this->random_strings(5);
+            $fps_data = $this->fps_data($fps_ref_string);
+
+            if( !$fps_data ){
+                echo __("This payment method is only available for HKD payments",ITS_WPF_PLUGIN_ID);
+                return;
+            }
+
+            $qr_code_url = plugins_url('fps-qrcode.php', __FILE__)  . '?' . $this->urlencode_array($fps_data);
+            
+            if ( $this->description ) {
+                echo wpautop( wp_kses_post( $this->description ) );
+            }
+
+            echo '<fieldset id="wc-' . esc_attr( $this->id ) . '-cc-form" class="wc-credit-card-form wc-payment-form" style="background:transparent;">';
+            
+            ?>
+            <div class="form-row form-row-wide">
+                FPS id: <strong><?php echo $this->account_fps_id ?></strong>
+            </div>
+
+            <?php
+
+            if ($this->ask_to_pay === 'no') {?>
+                <div class="form-row form-row-wide">
+                    <label>FPS Payment Reference <span class="required">*</span><br><small><?php echo $this->fps_payment_reference_guide?></small></label>
+                    <input id="its_wpf_payment_ref" name="its_wpf_payment_ref" type="text" autocomplete="off" value="<?php echo $fps_ref_string ?>">
+                </div>
+            <?php
+            }else{?>
+                <input id="its_wpf_payment_ref"  name="its_wpf_payment_ref" type="hidden" value="<?php echo $fps_ref_string ?>">
+            <?php
+            }
+            ?>
+            <div class="form-row form-row-wide" style="text-align: center;">
+                <img src="<?php echo $qr_code_url?>">
+            </div>
+
+            
+            <?php    
+            
+            echo '<div class="clear"></div></fieldset>';
+        }
+
+        public function validate_fields() {
+ 
+            if( empty( $_POST[ 'its_wpf_payment_ref' ]) ) {
+                wc_add_notice(  __('Payment reference is required!',ITS_WPF_PLUGIN_ID), 'error' );
+                return false;
+            }else{
+                $trimmed = preg_replace('/\s+/', '', sanitize_text_field($_POST[ 'its_wpf_payment_ref' ]) );
+
+                if (strlen($trimmed) === 0) {
+                    wc_add_notice(  __('Invalid payment reference!',ITS_WPF_PLUGIN_ID), 'error' );
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public function process_payment( $order_id ) {
+            global $woocommerce;
+            $order = new WC_Order( $order_id );
+
+            $order->update_status('on-hold', __( 'Awaiting manual confirmation of FPS payment.', ITS_WPF_PLUGIN_ID ));
+            $order->set_transaction_id(sanitize_text_field($_POST[ 'its_wpf_payment_ref' ]));
+            $order->save();
+
+            $woocommerce->cart->empty_cart();
+
+            return array(
+                'result' => 'success',
+                'redirect' => $this->get_return_url( $order )
+            );
+        }
+
+        private function random_strings($length_of_string) { 
+      
+            // md5 the timestamps and returns substring 
+            // of specified length 
+            return substr(md5(time()), 0, $length_of_string); 
+        } 
+    }
+}
+
+
+
+
+
